6aa2542ccb5b17014ccecda25661e03696b07088
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-25 11:40:33 +0900

    chore: build

---
+++ dist/types/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Adds comma's to a number
+ *
+ * @export
+ * @param {(string | number)} num the number to commafy
+ * @param {{ stripDecimals?: boolean, spacedDecimals?: boolean, thousandsComma?: boolean, K?: boolean }} [options] By default thousandsComma is enabled, if disabled it shows `1000` without comma (but `10,000` with)
+ * @returns {string} eg. '1,000,000'
+ */
+export declare function commafy(num: number, options?: {
+    stripDecimals?: boolean;
+    spacedDecimals?: boolean;
+    thousandsComma?: boolean;
+    K?: boolean;
+}): string;

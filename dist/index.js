6e227694a44feec06ef1cc21738a3e1683cf69c3
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:54:40 +1200

    chore: rebuild dist

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,17 @@
 /**
  * Adds comma's to a number
  *
+ * @param {string | number} num The number to commafy
+ * @param {{
+ *   stripDecimals?: boolean
+ *   spacedDecimals?: boolean
+ *   thousandsComma?: boolean
+ *   K?: boolean
+ * }} [options]
+ *   By default thousandsComma is enabled, if disabled it shows `1000` without comma (but `10,000`
+ *   with)
+ * @returns {string} Eg. '1,000,000'
  * @export
- * @param {(string | number)} num the number to commafy
- * @param {{ stripDecimals?: boolean, spacedDecimals?: boolean, thousandsComma?: boolean, K?: boolean }} [options] By default thousandsComma is enabled, if disabled it shows `1000` without comma (but `10,000` with)
- * @returns {string} eg. '1,000,000'
  */
 export function commafy(num, options) {
     const { stripDecimals = false, spacedDecimals = false, thousandsComma = true, K = false, } = options || {};
